43c08b163df674ad73496c10608e9a2ed50f8a94
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2024-07-02 19:09:20 +0530

    Made lower(name) as index

--- src/db/migrations/20240702041107_001_create_schema_users.js
+++ src/db/migrations/20240702041107_001_create_schema_users.js
@@ -5,7 +5,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments('id').primary().notNullable();
-    table.string('name').index().notNullable();
+    table.string('name').notNullable();
     table.string('email').unique().notNullable();
     table.string('phone_no').unique().notNullable();
     table.string("password").notNullable();
